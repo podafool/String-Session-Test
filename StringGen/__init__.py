0d34d8e73e450d41899e3d302cf2c18b3f14c6ba
podafool <167709327+podafool@users.noreply.github.com>
2024-05-01 19:41:35 +0530

    Update __init__.py
    mongo = MongoCli(config.MONGO_DB_URI)

--- StringGen/__init__.py
+++ StringGen/__init__.py
@@ -1,7 +1,7 @@
 import logging
 import pyromod
 
-from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+from motor.motor_asyncio import AsyncIOMotorClient
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 
@@ -17,9 +17,6 @@ logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("oldpyro").setLevel(logging.ERROR)
 logging.getLogger("telethon").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
-
-mongo = MongoCli(config.MONGO_DB_URI)
-db = mongo.StringGen
 
 
 class Anony(Client):
