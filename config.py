f541a9c549bd59234fe26a3dc87d990b094ed5f2
podafool <167709327+podafool@users.noreply.github.com>
2024-05-01 19:38:22 +0530

    Update config.py
    removed line 12 mongo

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 1356469075))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Idhayann")
